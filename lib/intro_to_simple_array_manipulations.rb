0f70003220ff0dedff92ac2e7d4a8d4f0c1e25b9
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-09 17:23:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,4 +18,8 @@ def using_delete(instructors, no_offense_steven)
   no_offense_steven = instructors.delete("Steven")
 end
 
-def using_delete_at
+def using_delete_at(famous_robots, deleted_robot)
+  deleted_robot = famous_robots.delete_at(famous_robots, 2)
+end
+  
+end
